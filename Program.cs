7e3af53c7435bab393528997fbd55a7dee9f1ed9
robbfreitas <robertocompromissos@gmail.com>
2022-12-07 18:03:37 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using Course;
+using System.Globalization;
+
+namespace SistemaDeEstoquePOOcomCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Entre com os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Produto p = new Produto(nome, preco);
+
+            Produto p2 = new Produto { 
+                Nome = "TVV",
+                Preco = 100.00,
+                Quantidade = 10
+            };
+
+            Console.WriteLine();
+            Console.Write("Dados do produto: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
